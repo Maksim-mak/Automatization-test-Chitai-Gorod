3efd3ff47e0a9ceafb9010f29c84c2e3fd143921
Maksim-Mak <maksimskyproraven@gmail.com>
2025-08-15 09:32:45 +0500

    Дополнение отчеТА

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 UI_url = "https://www.chitai-gorod.ru"
 API1_url = "https://web-agr.chitai-gorod.ru/web/api/v1/cart/product"
 API2_url = "https://web-agr.chitai-gorod.ru/web/api/v1/cart"
-bearer_token = ("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
-                "eyJleHAiOjE3NTUxMDIxMDcsImlhdCI6MTc1NDkzNDEwNywia"
-                "XNzIjoiL2FwaS92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjI1"
-                "Y2JlMDQzMmVkYzVmMTYyMmRiNTkyODAzN2ViMTlhYTY5OTkyNDZ"
-                "kMDhjYjJmOGIxYmZiYTU1ZDA1MWU4YzEiLCJ0eXBlIjoxMH0.yUb"
-                "8VR3ZEO4_vfYxCmDdijx4mVlnlPtT4skmff-HSX0")
-User_Agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Apple"
-              "WebKit/537.36 (KHTML, like Gecko) "
+bearer_token = ("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAi"
+                "OjE3NTUzOTk3NzksImlhdCI6MTc1NTIzMTc3OSwiaXNzIjoiL2Fwa"
+                "S92MS9hdXRoL2Fub255bW91cyIsInN1YiI6IjY1MjdiMTMyMzhjNG"
+                "VkYzllMDk3MzMyNTY2MTgwNThhOTA3MTYxMDM2OTBjNDQ2MWZlZDNj"
+                "NGExNTI0MjRhYmUiLCJ0eXBlIjoxMH0.noH1p2nZVx1Gx5aRp2QbwA"
+                "g66V7D8Q_wYTs2af2V43I")
+User_Agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+              "AppleWebKit/537.36 (KHTML, like Gecko) "
               "Chrome/138.0.0.0 Safari/537.36")
 User_ID = "1e199617-16ef-4c74-bc14-c9fba990cd9c"
